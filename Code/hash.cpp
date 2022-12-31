ccce9f137e04e9106495d9285068f790cd8810fa
Venen Jean <102526504+Savageboitim13@users.noreply.github.com>
2022-12-31 08:54:54 +0100

    Create hash.cpp

---
+++ Code/hash.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <openssl/sha.h>
+#include <cstring>
+#include <array>
+#include "..\Files\main.cpp"
+
+std::array<unsigned char, 32ULL> hash(const std::string& message);
+void writeHash(const std::array<unsigned char, 32ULL>& hash);
+
+std::array<unsigned char, 32ULL> hash(const std::string& message) {
+    // The message to be hashed
+    const char* notHashed = message.c_str();
+
+    // Compute the SHA-256 hash of the message
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, message.data(), message.size());
+    SHA256_Final(hash, &sha256);
+
+    // unsigned char[32] -> std::array<unsigned char, 32ULL>
+    std::array<unsigned char, 32ULL> result;
+    std::copy(hash, hash + 32, result.begin());
+
+    // Print the hash
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        std::cout << std::hex << (int)hash[i];
+    std::cout << std::endl;
+
+    return result;
+}
